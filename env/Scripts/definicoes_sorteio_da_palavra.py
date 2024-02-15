15604bc5e668575ab41767daab4400f59ab84214
eduardo2580 <118479773+eduardo2580@users.noreply.github.com>
2024-02-15 05:26:55 -0300

    Nothing

---
+++ env/Scripts/definicoes_sorteio_da_palavra.py
@@ -0,0 +1,4 @@
+#!C:\Users\Eduardo\jogo-da-forca\env\Scripts\python.exe
+# EASY-INSTALL-SCRIPT: 'jogo-da-forca==0.1.0','definicoes_sorteio_da_palavra.py'
+__requires__ = 'jogo-da-forca==0.1.0'
+__import__('pkg_resources').run_script('jogo-da-forca==0.1.0', 'definicoes_sorteio_da_palavra.py')
--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'jogo-da-forca',
-    version = '0.1.0',
-    author = 'Eric',
-    author_email = 'ericshantos13@gmail.com',
-    description = 'Minha primeira iniciativa em Python: um jogo-da-forca',
-    url = 'https://github.com/ericshantos/jogo-da-forca',
-    packages = find_packages(where = 'src'),
-    package_dir = {'':'src'},
+    name='jogo-da-forca',
+    version='0.1.0',
+    author='Eric',
+    author_email='ericshantos13@gmail.com',
+    description='Minha primeira iniciativa em Python: um jogo-da-forca',
+    url='https://github.com/ericshantos/jogo-da-forca',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
     install_requires=[
-        'forca',
-        'sorteio_palavra',
+        # Add any external dependencies here if needed
     ],
-    scripts = [
-        'forca/definicoes_da_forca.py',
-        'sorteio_palavra/definicoes_sorteio_da_palavra'
-    ]
+    scripts=[
+        'src/forca/definicoes_da_forca.py',
+        'src/sorteio_palavra/definicoes_sorteio_da_palavra.py',
+    ],
 )
