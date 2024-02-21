b3ae58521bcd60c80bc032c036d4d9a648f0bb9c
Eric Santos <118479896+ericshantos@users.noreply.github.com>
2024-02-21 16:47:55 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,21 +1,36 @@
+"""
+This module implements a simple hangman game and related utility functions.
+"""
+
 from forca.definicoes_da_forca import Forca
 import os
-        
-def limpador(booleano):
 
-    if (booleano):
+def limpador(booleano):
+    """
+    Clears the console screen based on the operating system.
+
+    Parameters:
+    - booleano (bool): A boolean value indicating whether to pause after clearing the screen.
+    """
+    if booleano:
         input('Precione ENTER para continuar...')
         os.system('cls' if os.name == 'nt' else 'clear')
     else:
         os.system('cls' if os.name == 'nt' else 'clear')
 
 def impressao_mensagem_fim_de_jogo(resultado_acertou, resultado_enforcou, a_palavra_secreta):
+    """
+    Prints the end-of-game message based on the game result.
 
+    Parameters:
+    - resultado_acertou (bool): Indicates whether the player won the game.
+    - resultado_enforcou (bool): Indicates whether the player lost the game.
+    - a_palavra_secreta (str): The secret word for the game.
+    """
     limpador(False)
 
-    if (resultado_acertou):
-
-        print('Parabéns, você ganhou! \n')
+    if resultado_acertou:
+        print('Parabéns! Você ganhou!\n')
         print(r"       ___________      ")
         print(r"      '._==_==_=_.'     ")
         print(r"      .-\\:      /-.    ")
@@ -26,9 +41,8 @@ def impressao_mensagem_fim_de_jogo(resultado_acertou, resultado_enforcou, a_pala
         print(r"           ) (          ")
         print(r"         _.' '._        ")
         print("         '-------'       \n")
-
-    elif (resultado_enforcou):
-
+            
+    elif resultado_enforcou:
         print('Puxa, você foi enforcado!  \n')
         print(r"    _______________         ")
         print(r"   /               \        ")
@@ -50,75 +64,77 @@ def impressao_mensagem_fim_de_jogo(resultado_acertou, resultado_enforcou, a_pala
         print(f'A palavra era {a_palavra_secreta} \n')
 
 def main():
-
-    # Clear the content given above
+    """
+    Main function to execute the hangman game.
+    """
+    # Clear the console
     limpador(False)
 
-    # Hangman game presentation
+    # Game introduction
     print('***************************************************************')
-    print('*************** Bem-Vindo ao jogo da forca ********************')
-    print('*************************************************************** \n')
+    print('*************** Bem-vindo ao jogo-da-forca ********************')
+    print('***************************************************************\n')
 
-    # Updating game stages
+    # Update game stages
     limpador(True)
 
-    # Presentation of the game rules
-    print('*********************** Regras do Jogo ****************************')
+    # Game rules presentation
+    print('*********************** Regras do jogo ****************************')
 
-    # Clarifying the rules of the game, how it will work.
-    print('Como funciona?? \n')
-    print(
-        'O jogo da forca consiste em adivinha uma palavra sorteada. Você só poderá tentar adivinha por \n tentativas, chutando letras a sua escolha.')
-    print('As letras acertadas seram integradas á forca, da qual você deve acerta a palavra para ganhar o jogo. \n')
+    # Clarifying the rules of the game
+    print('Como funciona?\n')
+    print('O jogo da forca consiste em adivinhar uma palavra escolhida aleatoriamente.')
+    print('Você só pode tentar adivinhar por 7 tentativas escolhendo letras.')
+    print('As letras corretamente adivinhadas serão integradas à forca, da qual você deve adivinhar a palavra para vencer o jogo.\n')
     print('ATENÇÃO!!!!!')
-    print('Você perde se errar um total de 7 chutes. \n')
-    print('BOM JOGO :) !! \n')
+    print('Você perde se fizer um total de 7 palpites errados.\n')
+    print('DIVIRTA-SE :) !!\n')
 
-    # Updating game stages
+    # Update game stages
     limpador(True)
 
-    # Game logic
+    # Game logic loop
     while True:
-
-        # Strength
+        # Set up hangman
         forca = Forca()
 
-        # loop validation variables
+        # Flags for game status
         acertou, enforcou = False, False
 
-        while (not acertou and not enforcou):
-            # Clears previously displayed content
+        while not acertou and not enforcou:
+            # Clear the console
             limpador(False)
 
-            # Gallows display and relevant information
+            # Display hangman and related information
             forca.layout_forca()
 
-            # Player shot capture
-            chute = input('Qual é o seu chute? \n')
+            # Player's guess
+            chute = input('What is your guess?\n')
 
-            # Determines if the guess corresponds to any letter in the word
+            # Check if the guess corresponds to any letter in the word
             forca.apuracao_da_forca(chute)
 
-            # Conditions for losing or winning the game
+            # Check win or lose conditions
             acertou = '_' not in forca.quant_lacunas
             enforcou = forca.erros == 7
 
-        # Printing the end of game message
+        # Print end-of-game message
         impressao_mensagem_fim_de_jogo(acertou, enforcou, forca.sorteador.palavra_sorteada)
 
-        # Updating game stages
+        # Update game stages
         limpador(True)
 
-        # Checks whether the player who plays again
-        jogar_denovo = input('Deseja jogar novamente?? (s/n): \n')
+        # Check if the player wants to play again
+        jogar_denovo = input('Desseja jogar novamente?? (s/n):\n')
 
         # If not, end the game
         if jogar_denovo != 's':
             break
 
-            # Clears previously displayed content
+            # Clear the console
             limpador(False)
             print('Obrigado por jogar ;)')
 
 if __name__ == '__main__':
     main()
+
