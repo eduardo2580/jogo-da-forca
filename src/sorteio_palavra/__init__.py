90399b32f663e186f75e63b73ad1dd2e43a18f96
Eric Santos <118479896+ericshantos@users.noreply.github.com>
2024-03-04 16:12:28 -0300

    Update __init__.py

---
+++ src/sorteio_palavra/__init__.py
@@ -0,0 +1,16 @@
+"""
+Definições sorteio da palavra secreta
+
+Esse pacote é responsável por fazer o sorteio de uma palavra através de uma lista pré-establecida.
+
+Responsabilidades:
+
+- Realizar o sorteio de uma palavra;
+- Retorna a palavra secreta;
+
+Métodos:
+
+- sortear_palavra(): sorteia a palavra.
+"""
+
+__version__ = '1.0.0'
