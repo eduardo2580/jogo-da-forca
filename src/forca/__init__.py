f2fab1834024287ec445c509140c07a619d36aed
Eric Santos <118479896+ericshantos@users.noreply.github.com>
2024-03-04 16:12:52 -0300

    Update __init__.py

---
+++ src/forca/__init__.py
@@ -0,0 +1,19 @@
+"""
+Definições da Forca
+
+Esse pacote é responsável definir as configurações da forca.
+
+Responsabilidades:
+
+- Controlar a marcação de erros;
+- Verificar  se os chutes do usuário correspondem a uma letra;
+- Exibição da forca.
+
+Métodos:
+
+- desenho_forca(): molda a forca de acordo com a palavra sorteada.
+- layout_forca(): exibir a pontuação, a forca e as letras chutadas.
+- apuracao_da_forca(): verifica se o chute do usuário corresponde a alguma letra da palavra.
+"""
+
+__version__ = '1.0.0'
